e461a84d8184b84789f6d88647e344bde88e4134
Nik Chankov <nik@chankov.net>
2024-12-04 13:31:40 +0000

    add php-gd to the mix

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@ sudo apt upgrade
 sudo apt install apache2
 sudo apt install mysql-server
 sudo apt install php libapache2-mod-php php-mysql
+sudo apt install php-gd
 #install certbot
 sudo snap install --classic certbot
 sudo ln -s /snap/bin/certbot /usr/bin/certbot
