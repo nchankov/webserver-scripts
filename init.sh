b19653a17b4cb4c44374e787117c66abd9ccab08
Nik Chankov <nik@chankov.net>
2025-05-19 23:02:39 +0100

    Update init.sh
    adding php-xml

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@ sudo apt upgrade
 sudo apt install apache2
 sudo apt install mysql-server
 sudo apt install php libapache2-mod-php php-mysql php-curl
+sudo apt install php-xml
 sudo apt install php-gd
 #install certbot
 sudo snap install --classic certbot
